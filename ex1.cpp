d78a513ffbeddb4c66a50416368dc6dde29791fc
hieu2k1 <hieu2k1@users.noreply.github.com>
2019-09-19 15:34:12 +0700

    ex1

---
+++ ex1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int a,b,c,d;
+	printf("\nnhap gia tri a: ");
+	scanf("%d",&a);
+	printf("\nnhap gia tri b: ");
+	scanf("%d",&b);
+	printf("\nnhap gia tri c: ");
+	scanf("%d",&c);
+	printf("\nhap gia tri d: ");
+	scanf("%d",&d);
+	int S;
+	S=a+b+c+d;
+	printf("\nket qua cua S=%d",S);
+	return 0;
+	
+}
