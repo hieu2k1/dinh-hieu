0cf56a299b8ca97c0c44934b2ca570e49f858cfb
hieu2k1 <54581408+hieu2k1@users.noreply.github.com>
2020-05-09 00:42:44 +0700

    bt

---
+++ tong,hieu,tich,thuong.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b,c,d,f,g;
+
+	printf("\nnhap so thu 1: ");
+	scanf("%d",&a);
+	printf("\nnhap so thu 2: ");
+	scanf("%d",&b);
+	
+	c=a+b;
+	printf("\ntong 2 so la %d",c);
+	
+	d=a-b;
+	printf("\nhieu 2 so la %d",d);
+	
+	f=a*b;
+	printf("\ntich 2 so la %d",f);
+	
+	g=a/b;
+	printf("\nthuong 2 so la %d",g);
+	return 0;
+}
