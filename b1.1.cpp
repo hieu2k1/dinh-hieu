9ed3d826ec65f3e7b88e4e49e8b9e6ef0737321d
hieu2k1 <54581408+hieu2k1@users.noreply.github.com>
2020-05-09 00:36:03 +0700

    Add files via upload

--- b1.1.cpp
+++ b1.1.cpp
@@ -2,18 +2,31 @@
 
 int main()
 {
-	int n;
-	printf ("nhap so nguyen n: ");
-	scanf("%d",&n);
-	if(n>0){
-		printf("%d la so nguyen duong\n",n);
-	}
-	if(n<0){
-		printf("%d la so nguyen am\n",n);
-	}
-	if(n==0){
-		printf("%d la so khong duong khong am\n",n);
-	}
+	char abc;
+	printf("gia tri cua abc: ");
+	scanf("%c",&abc);
+	int xyz;
+	printf("gia tri cua xyz: ");
+	scanf("%d",&xyz);
+	float length;
+	printf("gia tri cua length: ");
+	scanf("%f",&length);
+	double area;
+	printf("gia tri cua area: ");
+	scanf("%lf",&area);
+	long liteyrs;
+	printf("gia tri cua liteyrs: ");
+	scanf("%d",&liteyrs);
+	short arm;
+	printf("gia tri cua arm: ");
+	scanf("%d",&arm);
 	
+	printf("\ngtri char la %c");
+	printf("\ngtri xyz la %d");
+	printf("\ngtri length la%f");
+	printf("\ngtri aea la %lf");
+	printf("\ngtri liteyrs la %d");
+	printf("\ngtri arm la %d");
 	return 0;
+	
 }
