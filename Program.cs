a47e0bae85824a3ed7cc667f34305f3a9e661dff
hieu2k1 <54581408+hieu2k1@users.noreply.github.com>
2020-05-20 08:01:44 +0700

    Lab04

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace Lab01
+namespace Lab04
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Circle cl = new Circle(2.45,"Blue", 23);
+            Console.WriteLine("Circle before change: " + c1.ToString());
+
+            c1.PColor = "red";
+            c1.PWeight = 2.56;
+
+            Console.WriteLine("Circle after change: " + c1.ToString());
+            Console.ReadLine();
         }
     }
 }
