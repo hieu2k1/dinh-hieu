13f900ec603a63a24c731a227e6cb5e876183133
hieu2k1 <54581408+hieu2k1@users.noreply.github.com>
2020-05-29 02:12:01 +0700

    lallab09

---
+++ Departmen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Code10
+{
+    class Departmen : IEnumerable
+    {
+        string[] departmentNames = { "Marketing", "Finace", "Information Technology", "Human Resources" };
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < departmentNames.Length; i++)
+            {
+                yield return departmentNames[i];
+            }
+        }
+        static void Main(string[] args)
+        {
+            Departmen objDepartment = new Departmen();
+            Console.WriteLine("Department Names");
+            Console.WriteLine();
+            foreach (string str in objDepartment)
+            {
+                Console.WriteLine(str);
+            }
+        }
+        }
+    }
