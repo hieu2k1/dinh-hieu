4c1889d3c916e24d6582ce0c5dd61640f7a49596
hieu2k1 <54581408+hieu2k1@users.noreply.github.com>
2020-05-29 11:21:29 +0700

    LAB10(C#)

---
+++ EventProgra.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SampleApp
+{
+    public delegate string MyDel(string str);
+
+    class EventProgra
+    {
+        event MyDel MyEvent;
+
+        public EventProgra()
+        {
+            this.MyEvent += new MyDel(this.WelcomeUser);
+
+        }
+        public string WelcomeUser(string username)
+        {
+            return "Welcome" + username;
+        }
+        static void Main(string [] args)
+        {
+            EventProgra obj1 = new EventProgra();
+            string result = obj1.MyEvent("Tutorials Point");
+            Console.WriteLine(result);
+        }
+          
+    }
+}
