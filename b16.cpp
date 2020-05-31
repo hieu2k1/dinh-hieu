165d3f6f0478f60fc50765e3257c8b0f00ba9638
hieu2k1 <54581408+hieu2k1@users.noreply.github.com>
2020-05-31 12:35:26 +0700

    la10funtion

---
+++ b16.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int argc, char *argv[]) {
+	int a, b, c, sum;
+	printf("\nEnter any three numbers: ");
+	scanf("%d %d %d", &a, &b, &c);
+	sum = calculatesum(a, b, c);
+	printf("\nSum = %d", sum);
+	}
+	calculatesum(int x, int y, int z)
+	{
+		int d;
+		d = x + y + z;
+		return (d);
+	}
